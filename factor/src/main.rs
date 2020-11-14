28041e87457703545f3feeb4f22fe015f08d8c9b
zombie110year <zombie110year@outlook.com>
2020-11-14 22:25:09 +0800

    功能（factor）：用 Eratosthenes 筛法和辗转相除法分解质因数
    1. 问题：无法分解 10^9 数量级的数字（内存不足）

--- factor/src/main.rs
+++ factor/src/main.rs
@@ -56,5 +56,100 @@ fn check_and_print_factor(n: String) {
 
 // todo
 fn factor(n: u64) -> Vec<u64> {
-    return vec![n];
+    if n < 4 {
+        return vec![n];
+    } else {
+        let map = eratosthenes(n as usize);
+        let mut ans = Vec::new();
+        let mut n = n;
+
+        while n != 1 {
+            for p in map.iter() {
+                let q = gcd(n, *p);
+                if q != 1 {
+                    ans.push(q);
+                    n /= q;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+// ANCHOR: gcd
+fn gcd(a: u64, b: u64) -> u64 {
+    let (mut a, mut b) = if a < b { (b, a) } else { (a, b) };
+    while a % b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    return b;
+}
+// ANCHOR_END: gcd
+
+// ANCHOR: eratosthenes
+/// Eratosthenes 筛法求 n 以内的素数表
+/// + 时间复杂度 $O(n^{\frac{3}{2}})$
+/// + 空间复杂度 $O(n)$
+fn eratosthenes(n: usize) -> Vec<u64> {
+    let mut map = vec![true; n + 1];
+    map[0] = false;
+    map[1] = false;
+    // O(\sqrt{n})
+    for i in 2..=isqrt(n) {
+        if map[i] {
+            // O(n)
+            for j in 2..=(n / i) {
+                map[j * i] = false;
+            }
+        }
+    }
+    // O(n)
+    map.iter()
+        .enumerate()
+        .filter(|(_i, b)| **b)
+        .map(|(i, _b)| i as u64)
+        .collect()
+}
+
+fn isqrt(n: usize) -> usize {
+    if n < 2 {
+        n
+    } else {
+        let r = 2 * isqrt(n / 4);
+        if (r + 1) * (r + 1) > n {
+            r
+        } else {
+            r + 1
+        }
+    }
+}
+// ANCHOR_END: eratosthenes
+
+#[cfg(test)]
+#[test]
+fn test_eratosthenes() {
+    assert_eq!(
+        eratosthenes(100),
+        vec![
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+            89, 97
+        ]
+    )
+}
+
+#[cfg(test)]
+#[test]
+fn test_isqrt() {
+    assert_eq!(isqrt(0), 0);
+    assert_eq!(isqrt(1), 1);
+    assert_eq!(isqrt(2), 1);
+    assert_eq!(isqrt(3), 1);
+    assert_eq!(isqrt(4), 2);
+    for i in 1..256 {
+        assert_eq!(isqrt(256 * 256 + i), 256);
+    }
 }
