4176a887d6a67e2d30568deb1952720d8e4e88b8
zombie110year <zombie110year@outlook.com>
2020-11-15 13:24:43 +0800

    功能（prime）：在 prime 中实现埃氏筛法

---
+++ prime/src/lib.rs
@@ -0,0 +1,63 @@
+//! # libprime
+//!
+//! 与素数相关的算法
+
+/// # Eratosthenes 筛法
+///
+/// 求 $[2, n)$ 区间内的素数
+/// + 时间复杂度 $O(n^{\frac{3}{2}})$
+/// + 空间复杂度 $O(n)$
+// todo 自动推导 Vec<usize> -> Vec<T> 的转换
+
+// ANCHOR: eratosthenes
+pub fn eratosthenes<T>(n: T) -> Vec<usize>
+where
+    T: Into<usize> + From<usize>,
+{
+    let n = n.into();
+    let mut table = vec![true; n];
+    table[0] = false;
+    table[1] = false;
+    let mut head = 2;
+    'prime: while table[head] {
+        for notprime in (2 * head..n).step_by(head) {
+            table[notprime] = false;
+        }
+        for next_prime in (head + 1)..n {
+            if table[next_prime] {
+                head = next_prime;
+                continue 'prime;
+            }
+        }
+        break;
+    }
+
+    let primes = table
+        .iter()
+        .enumerate()
+        .filter(|(_, b)| **b)
+        .map(|(i, _)| i)
+        .collect();
+
+    return primes;
+}
+// ANCHOR_END: eratosthenes
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_eratosthenes() {
+        assert_eq!(
+            eratosthenes(100),
+            vec![
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
+                83, 89, 97
+            ]
+        )
+
+        // note: 空间复杂度 O(n)，因此数字过大时会崩溃
+        // eratosthenes(1000_000_000_000);
+    }
+}
